af813d4545117f9e36063e5816ba353af2e2cfe4
junghee.kim <wjdgml3834@naver.com>
2022-01-17 10:16:45 +0900

    ✨ sections 템플릿 추가

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("sections", {
+    title: "섹션 목록",
+  });
 });
 // index.hbs의 값을 layout.hbs안의 {{{body}}}안에 넣어주게 된다.
 // title을 바꾸면 바뀐다.
