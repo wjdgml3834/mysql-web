c3468c45c426f843e2a4f71eea70ef15edc302a7
junghee.kim <wjdgml3834@naver.com>
2022-01-21 07:44:41 +0900

    ✨ biz-simple 템플릿 추가 및 데이터베이스 섹션 추가

--- database/sql.js
+++ database/sql.js
@@ -24,8 +24,27 @@ const sql = {
     `);
     return rows;
   },
+  // SELECT * FROM sections 구문을 이용하여 이 쿼리로 모든 것을 선택한 다음에
+  //  그 결과들을 rows라는 곳으로 가져온다.
+
+  // 이제 섹션과 함께 Join된다
+  getBusinessesJoined: async (query) => {
+    const sqlQuery = `
+      SELECT * FROM sections S
+      LEFT JOIN businesses B
+        ON S.section_id = B.fk_section_id
+      WHERE TRUE
+        ${query.section ? "AND section_id = " + query.section : ""}
+        ${query.floor ? "AND floor = " + query.floor : ""}
+        ${query.status ? "AND status = '" + query.status + "'" : ""}
+      ORDER BY
+         ${query.order ? query.order : "business_id"}
+    `;
+    console.log(sqlQuery);
+
+    const [rows] = await promisePool.query(sqlQuery);
+    return rows;
+  },
 };
-// SELECT * FROM sections 구문을 이용하여 이 쿼리로 모든 것을 선택한 다음에
-//  그 결과들을 rows라는 곳으로 가져온다.
 
 module.exports = sql;
